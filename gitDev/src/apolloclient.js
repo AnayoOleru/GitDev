5c72e83fdd4ca7b799bb4d64686b55610ac31dc0
Anayo Oleru <anayo_oleru@outlook.com>
2019-06-24 09:17:41 +0100

    made changes to github bearer

--- gitDev/src/apolloclient.js
+++ gitDev/src/apolloclient.js
@@ -7,7 +7,7 @@ const client = new ApolloClient({
     link: new HttpLink({
       uri: 'https://api.github.com/graphql',
       headers: {
-        Authorization: 'bearer a6b21fd8447bff3e0d2156d67bd9db8e7c86792d'
+        Authorization: 'bearer 5fca7cbd157fcba7237500b52a7f353d753e5837'
       }
     }),
     cache: new InMemoryCache()
